91dbc859c18348394f7f2054a8a17934bdb93e26
Ryan Rossiter <nayr0788@gmail.com>
2019-08-22 12:26:49 -0400

    Force update gh-pages branch *sigh*

---
+++ precache-manifest.e1f19a04e6b630bf1e183c280bb4f25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffba10d131ee2a8c223955820b391c3a",
+    "url": "/ygk-kiosk/index.html"
+  },
+  {
+    "revision": "7ac99c90baa9c92c9df7",
+    "url": "/ygk-kiosk/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e19c68c71ee6edc734af",
+    "url": "/ygk-kiosk/static/css/main.8981d38a.chunk.css"
+  },
+  {
+    "revision": "7ac99c90baa9c92c9df7",
+    "url": "/ygk-kiosk/static/js/2.37bb8f54.chunk.js"
+  },
+  {
+    "revision": "e19c68c71ee6edc734af",
+    "url": "/ygk-kiosk/static/js/main.e48ca677.chunk.js"
+  },
+  {
+    "revision": "8fa5a257fbd126056425",
+    "url": "/ygk-kiosk/static/js/runtime~main.30472c18.js"
+  }
+]);
--- src/Defs.js
+++
@@ -1,6 +0,0 @@
-const DEBUG = process.env.NODE_ENV === 'development';
-
-export default {
-	DEBUG,
-	BASENAME: DEBUG? '' : '/ktown-kiosk',
-}
